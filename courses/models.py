af60b0f85ef72d899536028671a41480ef957ffe
Phil Bowden <phil.g.bowden@gmail.com>
2021-01-08 10:23:14 -0800

    Initial Commit

---
+++ courses/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Course(models.Model):
+	name = models.CharField(max_length = 200)
+	language = models.CharField(max_length = 100, default='Python', editable=True)
+	price = models.CharField(max_length = 10)
+
+
+	def __str__(self):
+		return self.name
+
+
+
+
